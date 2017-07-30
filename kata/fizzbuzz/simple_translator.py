db26d632b10568f37cbc0e8a7cdea44e5ad2dcf6
liuwill <liuwei_will@qq.com>
2017-07-30 19:26:40 +0800

    pass fizz and buzz

--- kata/fizzbuzz/simple_translator.py
+++ kata/fizzbuzz/simple_translator.py
@@ -1,2 +1,7 @@
 def translator(origin):
+    if origin % 3 == 0:
+        return "Fizz"
+    elif origin % 5 == 0:
+        return "Buzz"
+
     return str(origin)
--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -4,3 +4,9 @@ from kata.fizzbuzz import simple_translator
 def test_origin():
     assert simple_translator.translator(1) == "1"
     assert simple_translator.translator(2) == "2"
+
+def test_fizz():
+    assert simple_translator.translator(3) == "Fizz"
+
+def test_Buzz():
+    assert simple_translator.translator(5) == "Buzz"
